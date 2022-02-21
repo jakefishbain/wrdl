1f0f957c408689161b0f6f6a179f3ed79540e984
jakefishbain <87662007+jakefishbain-stripe@users.noreply.github.com>
2022-02-21 10:28:54 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+let guess;
+let result = [];
+let solution = 'coder'
+
+const process = require('process');
+const chalk = require('chalk');
+
+console.log('Guess a word...')
+
+process.stdin.on('data', data => {
+  console.log(`You typed ${data.toString()}`);
+  guess = data.toString();
+
+  guess.split('').forEach(function(guessLetter, index){
+    let solutionLetter = solution.charAt(index);
+    if (guessLetter === solutionLetter) {
+      result.push("green");
+    }
+    else if (solution.indexOf(guessLetter) != -1) {
+      result.push("yellow");
+    }
+    else {
+      result.push("grey");
+    }
+  })
+
+  let resultStr = '';
+
+  result.forEach(function(color, i) {
+    resultStr += chalk.bold.keyword(color)(guess[i])
+  });
+
+  console.log(resultStr)
+  process.exit();
+});
