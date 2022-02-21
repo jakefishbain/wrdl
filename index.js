3d3079c880d351dc016f0044c95faef710ea1db2
Jake Fishbain <jakefishbain@gmail.com>
2022-02-21 16:37:14 +0000

    formatting and wording

--- index.js
+++ index.js
@@ -8,28 +8,28 @@ const chalk = require('chalk');
 console.log('Guess a word...')
 
 process.stdin.on('data', data => {
-  console.log(`You typed ${data.toString()}`);
-  guess = data.toString();
+	console.log(`You guessed ${data.toString()}`);
+	guess = data.toString();
 
-  guess.split('').forEach(function(guessLetter, index){
-    let solutionLetter = solution.charAt(index);
-    if (guessLetter === solutionLetter) {
-      result.push("green");
-    }
-    else if (solution.indexOf(guessLetter) != -1) {
-      result.push("yellow");
-    }
-    else {
-      result.push("grey");
-    }
-  })
+	guess.split('').forEach(function(guessLetter, index) {
+		let solutionLetter = solution.charAt(index);
+		if (guessLetter === solutionLetter) {
+			result.push("green");
+		}
+		else if (solution.indexOf(guessLetter) != -1) {
+			result.push("yellow");
+		}
+		else {
+			result.push("grey");
+		}
+	})
 
-  let resultStr = '';
+	let resultStr = '';
 
-  result.forEach(function(color, i) {
-    resultStr += chalk.bold.keyword(color)(guess[i])
-  });
+	result.forEach(function(color, i) {
+		resultStr += chalk.bold.keyword(color)(guess[i])
+	});
 
-  console.log(resultStr)
-  process.exit();
+	console.log(resultStr)
+	process.exit();
 });
